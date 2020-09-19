585c8dd0f7303b7191da1d243e83f7e915ceb021
ncou <nicolas_courade@yahoo.fr>
2020-09-19 14:03:58 +0200

    update

---
+++ src/Bootloader/PublishHttpBootloader.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Http\Bootloader;
+
+use Chiron\Boot\Directories;
+use Chiron\Bootload\AbstractBootloader;
+use Chiron\PublishableCollection;
+
+final class PublishHttpBootloader extends AbstractBootloader
+{
+    public function boot(PublishableCollection $publishable, Directories $directories): void
+    {
+        $publishable->add(__DIR__ . '/../../config/http.php.dist', $directories->get('@config/http.php'));
+    }
+}
