c710c9aa1e8c113baae5b5a66ae7b118132d88cb
ncou <nicolas_courade@yahoo.fr>
2021-01-31 15:27:34 +0100

    update

--- src/Bootloader/PublishHttpBootloader.php
+++ src/Bootloader/PublishHttpBootloader.php
@@ -6,12 +6,12 @@ namespace Chiron\Http\Bootloader;
 
 use Chiron\Core\Container\Bootloader\AbstractBootloader;
 use Chiron\Core\Directories;
-use Chiron\PublishableCollection;
+use Chiron\Core\Publisher;
 
 final class PublishHttpBootloader extends AbstractBootloader
 {
-    public function boot(PublishableCollection $publishable, Directories $directories): void
+    public function boot(Publisher $publisher, Directories $directories): void
     {
-        $publishable->add(__DIR__ . '/../../config/http.php.dist', $directories->get('@config/http.php'));
+        $publisher->add(__DIR__ . '/../../config/http.php.dist', $directories->get('@config/http.php'));
     }
 }
--- src/Middleware/AllowedHostsMiddleware.php
+++ src/Middleware/AllowedHostsMiddleware.php
@@ -28,7 +28,6 @@ final class AllowedHostsMiddleware implements MiddlewareInterface
     public function __construct(HttpConfig $httpConfig, SettingsConfig $settingsConfig)
     {
         $this->allowedHosts = $httpConfig->getAllowedHosts();
-
         // Allow variants of localhost if ALLOWED_HOSTS list is empty and DEBUG is enabled.
         if ($settingsConfig->isDebug() && $this->allowedHosts === []) {
             // localhost and subdomain / IPv4 / IPv6 (brackets for URI use)
@@ -46,7 +45,14 @@ final class AllowedHostsMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $host = $request->getUri()->getHost();
+        // TODO : attention quand il y a un port dans le host cela ne fonctionne plus !!! il faut aussi faire une exception dédiée dans le cas ou le host est invalid :
+        // https://github.com/django/django/blob/a948d9df394aafded78d72b1daa785a0abfeab48/django/http/request.py#L135
+        // https://github.com/django/django/blob/a948d9df394aafded78d72b1daa785a0abfeab48/django/http/request.py#L143
+
+        // SPLIT METHOD : https://github.com/django/django/blob/a948d9df394aafded78d72b1daa785a0abfeab48/django/http/request.py#L640
+        // TESTS : https://github.com/django/django/blob/a948d9df394aafded78d72b1daa785a0abfeab48/tests/requests/tests.py#L814
+        [$host, $port] = $this->splitDomainPort($request->getUri()->getHost());
+
         // Check the 'Host' header value with the whitelist.
         if (! $this->isValidHost($host)) {
             // Represents an http 412 error code (request precondition failed).
@@ -80,5 +86,44 @@ final class AllowedHostsMiddleware implements MiddlewareInterface
     	}
 
     	return false;
+    }
+
+    /**
+     * Return a (domain, port) tuple from a given host.
+     * Returned domain is lowercased. If the host is invalid, the domain will be
+     * empty.
+     */
+    // TODO : autre exemple :    https://github.com/symfony/http-client/blob/b458d19fe834f055f4d50a9d0c85633a94023492/NativeHttpClient.php#L285
+    // TODO : améliorer le code, on a juste besoin du hostname et pas du port lors de l'utilisation de cette méthode, donc faire simplement une méthode getHostname() qui s'occupe de retourner uniquement le hostname on ne traitera pas la récupération du port. Attention au cas ou le hostname fini par un '.'
+    //https://serverfault.com/questions/803033/should-i-append-a-dot-at-the-end-of-my-dns-urls#:~:text=The%20dot%20at%20the%20end,a%20dot%20at%20the%20end.&text=This%20was%20documented%20in%20the,which%20ends%20in%20a%20dot.
+    //http://www.dns-sd.org/trailingdotsindomainnames.html
+    private function splitDomainPort(string $host): array
+    {
+        $host = strtolower($host);
+
+        if (! preg_match('/^([a-z0-9.-]+|\[[a-f0-9]*:[a-f0-9\.:]+\])(:\d+)?$/', $host)) {
+            return ['', ''];
+        }
+
+        // It's an IPv6 address without a port.
+        if (substr($host, -1) === ']') {
+            return [$host, ''];
+        }
+
+        $parts = explode(':', $host);
+        if (count($parts) === 2) {
+            $domain = $parts[0];
+            $port = $parts[1];
+        } else {
+            $domain = $parts[0];
+            $port = '';
+        }
+
+        // Remove a trailing dot (if present) from the domain.
+        if (substr($domain, -1) === '.') {
+            $domain = substr($domain, 0, -1);
+        }
+
+        return [$domain, $port];
     }
 }
